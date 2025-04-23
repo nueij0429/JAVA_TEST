27024dbfe67d0f8327033692b2af359a609080b2
nueij0429 <ssjjjieun0429@gmail.com>
2025-04-23 10:52:40 +0900

    java project

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+ * javac HelloWorld.java
+ * java HelloWorld
+ * Run ¥‹√‡≈∞: ctrl + f11
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello Java~!");
+	}
+}
