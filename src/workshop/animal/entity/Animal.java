4c1ac103b8806712139acfb3ea0cda40a2d47548
nueij0429 <ssjjjieun0429@gmail.com>
2025-04-24 11:19:52 +0900

    Cat 클래스까지 구현

--- src/workshop/animal/entity/Animal.java
+++ src/workshop/animal/entity/Animal.java
@@ -6,5 +6,11 @@ public abstract class Animal {
 	protected Animal(int legs) {
 		this.legs = legs;
 	}
+	
+	public abstract void eat();
+	
+	public void walk() {
+		System.out.println("동물은 " + legs + "발로 걷는다.");
+	}
 
 }
---
+++ src/workshop/animal/entity/Spider.java
@@ -0,0 +1,13 @@
+package workshop.animal.entity;
+
+public class Spider extends Animal {
+	public Spider() {
+		super(8);
+	}
+	
+	@Override
+	public void eat() {
+		System.out.println("Spider´Â ÀÛÀº ¹ú·¹¸¦ ¸Ô¾î¿ë!");
+		
+	}
+}
